6492c1a7c7023547df2654a0602508095345a9f3
dadaa <1692972759@qq.com>
2017-05-17 14:02:20 +0800

    提交下哈

--- my.js
+++ my.js
@@ -1,4 +1,5 @@
 var box=document.getElementsByClassName('box')[0];
+var timer=null;
 console.log(box)
 var i=0;
 function run(){
@@ -12,6 +13,25 @@ function run(){
     console.log(box.style.transform)
     
 }
-setInterval(function(){
+timer=setInterval(function(){
     run();
 },3000);
+box.onmouseover=function(e){
+    clearInterval(timer)
+}
+box.onmouseout=function(e){
+    timer=setInterval(function(){
+         run();
+    },3000);
+}
+
+//上面是轮播
+var time=document.getElementById('time');
+        gshi=new Date().getHours();
+        gfen=new Date().getMinutes();
+        gmiao=new Date().getSeconds();
+        var timer1=setInterval(function(){
+            var now=new Date();
+           
+           // console.log(shi-gshi+now.getHours(),fen-gfen+now.getMinutes(),miao-gmiao+now.getSeconds());
+        },100);
